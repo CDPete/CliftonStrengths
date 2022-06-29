6eda99e1daf09b943222182079f97e53b5ee3576
CDPete <cdpetersheim@gmail.com>
2022-06-29 16:19:14 -0500

    Can use lowercase versions now

--- script.js
+++ script.js
@@ -6,11 +6,16 @@
     // Check if same strength has been used
 
 function validateForm(event) {
-    const validStrengths = ["Achiever", "Activator", "Adaptability", "Analytical", "Arranger", "Belief", "Command", "Communication", 
-    "Competition", "Connectedness", "Consistency", "Context", "Deliberative", "Developer", "Discipline", 
-    "Empathy", "Focus", "Futuristic", "Harmony", "Ideation", "Includer", "Individualization", "Input", 
-    "Intellection", "Learner", "Maximizer", "Positivity", "Relator", "Responsibility", "Restorative", 
-    "Self-Assurance", "Significance", "Strategic", "Woo"]
+    //const validStrengths = ["Achiever", "Activator", "Adaptability", "Analytical", "Arranger", "Belief", "Command", "Communication", 
+    //"Competition", "Connectedness", "Consistency", "Context", "Deliberative", "Developer", "Discipline", 
+   // "Empathy", "Focus", "Futuristic", "Harmony", "Ideation", "Includer", "Individualization", "Input", 
+   // "Intellection", "Learner", "Maximizer", "Positivity", "Relator", "Responsibility", "Restorative", 
+    //"Self-Assurance", "Significance", "Strategic", "Woo"]
+
+    const validStrengths = ['achiever', 'activator', 'adaptability', 'analytical', 'arranger', 'belief', 'command', 'communication', 
+    'competition', 'connectedness', 'consistency', 'context', 'deliberative', 'developer', 'discipline', 
+    'empathy', 'focus', 'futuristic', 'harmony', 'ideation', 'includer', 'individualization', 'input', 'intellection', 
+    'learner', 'maximizer', 'positivity', 'relator', 'responsibility', 'restorative', 'self-assurance', 'significance', 'strategic', 'woo']
 
     var first = document.forms["form"]["First"].value;
     var second = document.forms["form"]["Second"].value;
@@ -33,8 +38,9 @@ function validateForm(event) {
         return false;
     }
     
-    if (!validStrengths.includes(first) || !validStrengths.includes(second) || !validStrengths.includes(third) || 
-        !validStrengths.includes(fourth) || !validStrengths.includes(fifth)) {
+    if (!validStrengths.includes(first.toLowerCase()) || !validStrengths.includes(second.toLowerCase()) || 
+        !validStrengths.includes(third.toLowerCase()) || !validStrengths.includes(fourth.toLowerCase()) || 
+        !validStrengths.includes(fifth.toLowerCase())) {
         alert("One or more of your strengths are invalid");
         event.preventDefault();
         return false;
